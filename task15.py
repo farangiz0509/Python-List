05e7a9c25b38ee92708c4cc080e6193023f5130c
farangiz0509 <farangiztolibova75@gmail.com>
2025-06-27 12:23:38 +0545

    done

---
+++ task15.py
@@ -0,0 +1,5 @@
+list = [1, 2, 3, 4, 5, 6, 7, 8, ]
+start = int(input("enter the frst num:"))
+end = int(input("enter the second num:"))
+result = list[start : end ]
+print(result)
--- task16.py
+++
@@ -1,5 +0,0 @@
-list = [1, 2, 3, 4, 5, 6, 7, 8, ]
-start = int(input("enter the frst num:"))
-end = int(input("enter the second num:"))
-result = list[start : end ]
-print(result)
